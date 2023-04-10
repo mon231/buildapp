4bf0e8e44c2a3045e670aec2fa8baa8d5023485b
Ariel Tubul <avarti765@gmail.com>
2023-04-10 22:28:03 +0300

    moved to package filetree

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "buildapp",
+    version = "1.0.1",
+    author = "Ariel Tubul",
+    description = "Apk builder script",
+    url = "https://github.com/mon231/buildapp/",
+    packages = setuptools.find_packages()
+)
