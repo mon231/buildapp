3c71672b3b2127e0e7e2b65bd0cf7a3eb979ecdb
Ariel Tubul <avarti765@gmail.com>
2023-04-27 21:46:27 +0300

    minor release

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import setuptools
 
+
 setuptools.setup(
     name = "buildapp",
-    version = "1.1.0",
+    version = "1.1.1",
     author = "Ariel Tubul",
     description = "Apk builder script",
     packages = setuptools.find_packages(),
     url = "https://github.com/mon231/buildapp/",
-    entry_points = {
-        'console_scripts': ['buildapp=buildapp.buildapp:main'],
-    }
+    entry_points = {'console_scripts': ['buildapp=buildapp.buildapp:main']}
 )
