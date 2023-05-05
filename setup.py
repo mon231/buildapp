43aa864b96c4dbf8ffb1dfa482b634ca0b6afc42
Ariel Tubul <avarti765@gmail.com>
2023-05-05 16:00:57 +0300

    Added new script (inorder to simplify fetching tools)

--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ CURRENT_FOLDER = Path(__file__).parent
 README_PATH = CURRENT_FOLDER / 'README.md'
 
 
-setuptools.setup(
-    name = "buildapp",
-    version = "1.1.3",
-    author = "Ariel Tubul",
-    description = "Apk builder script",
-    packages = setuptools.find_packages(),
-    long_description = README_PATH.read_text(),
-    url = "https://github.com/mon231/buildapp/",
-    long_description_content_type='text/markdown',
-    entry_points = {'console_scripts': ['buildapp=buildapp.buildapp:main']}
-)
+def main():
+    setuptools.setup(
+        name = "buildapp",
+        version = "1.1.3",
+        author = "Ariel Tubul",
+        description = "Apk builder script",
+        packages = setuptools.find_packages(),
+        long_description = README_PATH.read_text(),
+        url = "https://github.com/mon231/buildapp/",
+        long_description_content_type='text/markdown',
+        entry_points = {'console_scripts': ['buildapp=buildapp.buildapp:main', 'buildapp_fetch_tools=buildapp.download_tools:main']},
+        install_requires = ['requests']
+    )
+
+
+if __name__ == '__main__':
+    main()
