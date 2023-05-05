1e44ebb53df6f722ff85f5899dc5dc32a31bd11d
Ariel Tubul <avarti765@gmail.com>
2023-05-05 12:22:21 +0300

    Updated tools downloader

--- download_latest_tools.py
+++ download_latest_tools.py
@@ -1,45 +1,153 @@
 import io
 import re
 import sys
+import shutil
+import os.path
 import requests
 from pathlib import Path
 from zipfile import ZipFile
 from os.path import expanduser
 
-BUILD_TOOLS_PATH = Path(expanduser('~/.reltools/builadpp_build_tools'))
+
+DOWNLOAD_SUCCEEDED = 200
+
+APKTOOL_PATH = Path(expanduser('~/.reltools/third_party_apktool'))
+PLATFORM_TOOLS_PATH = Path(expanduser('~/.reltools/third_party_platform_tools'))
+BUILD_TOOLS_PATH = Path(expanduser('~/.reltools/third_party_android_build_tools'))
 
 BUILD_TOOLS_PATTERN = r'Build Tools, Revision (\d+)\.(\d+)\.(\d+)'
-BUILD_TOOLS_RELEASES = 'https://developer.android.com/tools/releases/build-tools'
-BUILD_TOOLS_DOWNLOAD_URL = 'https://dl.google.com/android/repository/build-tools_r{version}-{os}.zip'
+BUILD_TOOLS_RELEASES_URL = 'https://developer.android.com/tools/releases/build-tools'
 
-PLATFORM_TO_OS = {
+BUILD_TOOLS_DOWNLOAD_URL = 'https://dl.google.com/android/repository/build-tools_r{version}-{os}.zip'
+PLATFORM_TOOLS_DOWNLOAD_URL = 'https://dl.google.com/android/repository/platform-tools-latest-{os}.zip'
+
+APKTOOL_RELEASES_URL = 'https://bitbucket.org/iBotPeaches/apktool/downloads/'
+PLATFORM_TO_APKTOOL_WRAPPER_URL = {
+    'linux': 'https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool',
+    'darwin': 'https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/osx/apktool',
+    'win32': 'https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/windows/apktool.bat'
+}
+
+PLATFORM_TO_BUILD_TOOLS_OS = {
     'linux': 'linux',
     'darwin': 'macosx',
     'win32': 'windows',
 }
 
+PLATFORM_TO_PLATFORM_TOOLS_OS = {
+    'linux': 'linux',
+    'darwin': 'darwin',
+    'win32': 'windows'
+}
 
-def main():
-    platform_os = PLATFORM_TO_OS[sys.platform]
-    releases = requests.get(BUILD_TOOLS_RELEASES)
+class ToolsFetcher:
+    def __init__(self):
+        shutil.rmtree(BUILD_TOOLS_PATH, ignore_errors=True)
+        shutil.rmtree(PLATFORM_TOOLS_PATH, ignore_errors=True)
 
-    for major, minor, patch in re.findall(BUILD_TOOLS_PATTERN, releases.text):
-        download_responses = list(filter(
-            lambda response: response.status_code == 200,
-            (
-                requests.get(BUILD_TOOLS_DOWNLOAD_URL.format(version=f'{major}', os=platform_os)),
-                requests.get(BUILD_TOOLS_DOWNLOAD_URL.format(version=f'{major}.{minor}.{patch}', os=platform_os))
-            )
-        ))
+        self.__build_tools_os = PLATFORM_TO_BUILD_TOOLS_OS[sys.platform]
+        self.__platform_tools_os = PLATFORM_TO_BUILD_TOOLS_OS[sys.platform]
+        self.__build_tools_releases = requests.get(BUILD_TOOLS_RELEASES_URL).text
 
-        if download_responses:
+    def download_all_tools(self):
+        print('downloading build tools ...')
+        self.__download_build_tools()
+        ToolsFetcher.__unpack_subfolder(BUILD_TOOLS_PATH)
+
+        print('downloading platform tools ...')
+        self.__download_platform_tools()
+        ToolsFetcher.__unpack_subfolder(PLATFORM_TOOLS_PATH)
+
+        print('downloading apktool ...')
+        self.__download_apktool()
+
+        print('installation completed!')
+
+    def __download_build_tools(self):
+        for major, minor, patch in re.findall(BUILD_TOOLS_PATTERN, self.__build_tools_releases):
+            resp = requests.get(BUILD_TOOLS_DOWNLOAD_URL.format(version=f'{major}', os=self.__build_tools_os))
+
+            if resp.status_code != DOWNLOAD_SUCCEEDED:
+                resp = requests.get(BUILD_TOOLS_DOWNLOAD_URL.format(version=f'{major}.{minor}.{patch}', os=self.__build_tools_os))
+
+            if resp.status_code != DOWNLOAD_SUCCEEDED:
+                continue
+
             BUILD_TOOLS_PATH.mkdir(parents=True, exist_ok=True)
 
-            build_tools_zip = ZipFile(io.BytesIO(download_responses[0].content))
+            build_tools_zip = ZipFile(io.BytesIO(resp.content))
             build_tools_zip.extractall(str(BUILD_TOOLS_PATH))
-            break
-    else:
-        raise Exception('Error couldn\'t download build-tools')
+            return
+
+        raise RuntimeError('Error couldn\'t download build-tools')
+
+    def __download_platform_tools(self):
+        download_response = requests.get(PLATFORM_TOOLS_DOWNLOAD_URL.format(os=self.__platform_tools_os))
+
+        if download_response.status_code != DOWNLOAD_SUCCEEDED:
+            raise RuntimeError('Error couldn\'t download platform tools')
+
+        PLATFORM_TOOLS_PATH.mkdir(parents=True, exist_ok=True)
+
+        platform_tools_zip = ZipFile(io.BytesIO(download_response.content))
+        platform_tools_zip.extractall(str(PLATFORM_TOOLS_PATH))
+
+    def __download_apktool(self):
+        apktool_wrapper = requests.get(PLATFORM_TO_APKTOOL_WRAPPER_URL[sys.platform])
+
+        if apktool_wrapper.status_code != DOWNLOAD_SUCCEEDED:
+            raise RuntimeError('Couldn\'t download apktool wrapper')
+
+        apktool_releases = requests.get(APKTOOL_RELEASES_URL)
+        APKTOOL_URL_PATTERN = r'(/iBotPeaches/apktool/downloads/apktool_\d+\.\d+\.\d+\.jar)'
+
+        for download_path in re.findall(APKTOOL_URL_PATTERN, apktool_releases.text):
+            resp = requests.get('https://bitbucket.org' + download_path)
+
+            if resp.status_code != DOWNLOAD_SUCCEEDED:
+                continue
+
+            APKTOOL_PATH.mkdir(parents=True, exist_ok=True)
+
+            (APKTOOL_PATH / 'apktool.jar').write_bytes(resp.content)
+            (APKTOOL_PATH / ('apktool.bat' if sys.platform == 'win32' else 'apktool')).write_bytes(apktool_wrapper.content)
+
+            return
+
+        raise RuntimeError('Error couldn\'t download build-tools')
+
+    @staticmethod
+    def __get_subfolder(root: Path) -> Path:
+        subfolders = list(root.glob('./*'))
+
+        if len(subfolders) != 1:
+            raise RuntimeError('Error more than one folder')
+
+        return subfolders[0]
+
+    @staticmethod
+    def __unpack_subfolder(root: Path) -> str:
+        subfolder = ToolsFetcher.__get_subfolder(root)
+
+        for old_path in subfolder.rglob('*'):
+            if old_path.is_dir():
+                continue
+
+            fname_parts = list(old_path.parts)
+            fname_parts.remove(subfolder.name)
+
+            new_path = Path(os.path.join(*fname_parts))
+            new_path.parent.mkdir(exist_ok=True, parents=True)
+
+            print('Unpacking', new_path)
+            shutil.copy(old_path, new_path)
+
+        shutil.rmtree(subfolder, ignore_errors=True)
+
+
+def main():
+    tools_fetcher = ToolsFetcher()
+    tools_fetcher.download_all_tools()
 
 
 if __name__ == '__main__':
